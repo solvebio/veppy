b6d6982a6e16072abe1e323161740828c23627c6
Joseph Huttner <jhuttner@solvebio.com>
2017-03-07 20:43:41 +0000

    Switch to pickle.loads to avoid readline call in pickle.load

--- veppy/utils/data/__init__.py
+++ veppy/utils/data/__init__.py
@@ -1,10 +1,10 @@
 import os
 import gzip
-import pickle
+import cPickle
 
 data_dir = os.path.dirname(__file__)
 
 CGD_GENE_SYMBOLS_VERSION = "CGD/1.1.2-2015-09-24/CGD"
 with gzip.open(os.path.join(
         data_dir, 'cgd-1.1.2-2015-09-24-cgd.pickle.gz'), 'rb') as f:
-    CGD_GENE_SYMBOLS = pickle.load(f)
+    CGD_GENE_SYMBOLS = cPickle.loads(f.read())
